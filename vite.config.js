6aaecdf2dfe7cce1f9d501cffb68bed3eb5fa034
tusharR0 <tusharraut704@gmail.com>
2025-08-06 13:29:57 +0530

    Update for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/simple/', // ← new repo name
+  base: '/new/', // ← new repo name
   plugins: [react()],
 });
